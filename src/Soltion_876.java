82e5f3ef7ae5ae4fb9351ea6a219889376eb3cd6
Shriyansh Agarwal <agarwalshriyansh007@gmail.com>
2022-07-06 20:19:07 +0530

    876

---
+++ src/Soltion_876.java
@@ -0,0 +1,23 @@
+// 876. Middle of the Linked List
+
+//  Definition for singly-linked list.
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+// Approach: Fast and Slow Pointers
+// TC: O(n) SC:O(1)
+public class Soltion_876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head ,fast = head;
+        while (fast!= null && fast.next!=null){
+            slow=slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
