2e43536c1a3f4961de91c02de36c15ed816ea5ca
Shriyansh Agarwal <agarwalshriyansh007@gmail.com>
2022-07-14 13:41:20 +0530

    136

---
+++ src/Leetcode_136.java
@@ -0,0 +1,19 @@
+// 136. Single Number
+
+public class Leetcode_136 {
+    public static void main(String[] args) {
+        Solution_136 s = new Solution_136();
+        int nums[] = {4,1,2,1,2};
+        System.out.println(s.singleNumber(nums));
+    }
+}
+
+class Solution_136 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i= 0;i<nums.length;i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
