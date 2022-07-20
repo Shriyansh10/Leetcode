0a871e28bdd604e7ef1134bb2a4e55597f8b5c73
Shriyansh Agarwal <agarwalshriyansh007@gmail.com>
2022-07-20 10:48:53 +0530

    N-queen Optimized

--- src/Nqueen.java
+++ src/Nqueen.java
@@ -15,13 +15,16 @@ public class Nqueen {
 class Solution{
     static ArrayList<ArrayList<Integer>> nQueen(int n) {
         Solution s=new Solution();
-        int [][] matrix = new int [n][n];
+        int [][]matrix = new int [n][n];
+        int []rowhash = new int [n];
+        int []leftdiagonalhash = new int[2*n-1];
+        int []rightdiagonalhash = new int[2*n-1];
         ArrayList<ArrayList<Integer>> finalans = new ArrayList<>();
-        s.solve(matrix, 0, 0, finalans);
+        s.solve(matrix, 0, 0, finalans, rowhash, leftdiagonalhash, rightdiagonalhash);
         return finalans;
     }
     
-    protected void solve(int[][] matrix, int row, int col, ArrayList<ArrayList<Integer>> finalans){
+    protected void solve(int[][] matrix, int row, int col, ArrayList<ArrayList<Integer>> finalans, int []rowhash, int []leftdiagonalhash, int []rightdiagonalhash){
         if(col==matrix.length || row ==matrix.length){
             if(row==matrix.length){
                 ArrayList<Integer> ans = new ArrayList<>();
@@ -38,35 +41,32 @@ class Solution{
             return;
         }
         for(int i=0;i<matrix.length;i++){
-            if(isSafe(matrix, row, i)){
+            if(isSafe(matrix, row, i, rowhash, leftdiagonalhash, rightdiagonalhash)){
                 matrix[row][i] = 1;
-                // ans.add(col+1);
-                solve(matrix, row+1, i, finalans);
-                // ans.remove(ans.size()-1);
+                rowhash[i] = 1;
+                leftdiagonalhash[row - i + matrix.length -1] = 1;
+                rightdiagonalhash[2*matrix.length -2 - row - i] = 1;
+                solve(matrix, row+1, i, finalans, rowhash, leftdiagonalhash, rightdiagonalhash);
                 matrix[row][i]=0;
+                rowhash[i] = 0;
+                leftdiagonalhash[row - i + matrix.length -1] = 0;
+                rightdiagonalhash[2*matrix.length -2 - row - i] = 0;
             }
-            // return;
         }
     }
     
-    protected boolean isSafe(int [][]matrix, int row, int col){
-        // upper diagonal
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
-            if(matrix[i][j] == 1){
-                return false;
-            }
+    protected boolean isSafe(int [][]matrix, int row, int col, int []rowhash, int []leftdiagonalhash, int []rightdiagonalhash){
+        // left diagonal
+        if(leftdiagonalhash[row - col + matrix.length -1] == 1){
+            return false;
         }
         // up 
-        for(int i=row,j=col;i>=0 ;i--){
-            if(matrix[i][j] == 1){
-                return false;
-            }
+        if(rowhash[col] == 1){
+            return false;
         }
-        // lower diagonal
-        for(int i=row,j=col;i>=0 && j<matrix.length;i--,j++){
-            if(matrix[i][j] == 1){
-                return false;
-            }
+        // right diagonal
+        if(rightdiagonalhash[2*matrix.length -2 - row - col] == 1){
+            return false;
         }
         return true;
     }
